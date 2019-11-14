1c87de7f0a8e61e6906baacb63d72fb65bbbdec5
Erika Tsuchiya <tsuchiya.erika@synergy101.jp>
2019-11-14 19:51:49 +0900

    add main

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"net/http" 
+
+	"github.com/gorilla/mux"
+)
+
+
+func main()  {
+	r := mux.NewRouter()
+    // r.HandleFunc("/", HomeHandler)
+    // r.HandleFunc("/products", ProductsHandler)
+    // r.HandleFunc("/articles", ArticlesHandler)
+    http.Handle("/", r)
+}
